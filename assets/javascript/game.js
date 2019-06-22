6f459fbfe5f8b80c690384821b40f15688669955
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-06-22 16:43:52 -0400

    fixed onekeyup error(scipt being linked twice caused it), created function to pull random word from the word bank and put it in the randWord div

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,10 +3,15 @@ const wordBank = ['JavaScript', 'Python', 'Lua', 'Java', 'Perl', 'Swift', 'Ruby'
 
 let randWordGen = wordBank[Math.floor(Math.random() * wordBank.length)];
 
-let randWord = document.getElementById('randWord');
+let incLetters = document.getElementById('incLetters');
 
 //onkeyup function that changes the text in the random word div
 document.onkeyup = function(event) {
-    randWord.textContent = event.key;
+    incLetters.textContent = event.key;
 };
 
+function randWordGenFunc (){
+    randWord.textContent = randWordGen;
+}
+
+randWordGenFunc();
