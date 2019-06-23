4ae6bf25e7771b971d49de3b3275fdc9aed0eef7
DTHutton <48737212+DTHutton@users.noreply.github.com>
2019-06-22 23:43:29 -0400

    finshed parts of it. psuedocode for the rest. honestly kind of sucks overall

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,10 +9,13 @@ let word = wordBank[randNum];
 let underScore = [];
 let correctLetter = [];
 let incorrectLetter = [];
+let wins = 0;
 
 //dom
 let liveWord = document.getElementById('randWord');
-
+let winCount = document.getElementsByClassName('winCount');
+let lossCount = document.getElementsByClassName('lossCount');
+let incLetters = document.getElementById('incLetters');
 
 //code check
 console.log(word);
@@ -23,29 +26,54 @@ let generateUnderScore = () => {
         underScore.push('_');
     }
     return underScore;
+
 }
-//code check
-console.log(generateUnderScore());
+
+let applyIncorrectLetters = () => {
+    incorrectLetter.push('');
+    return incorrectLetter;
+}
+//applies underscores to current word.
+generateUnderScore();
+liveWord.textContent = underScore.join(' ');
+
 
 //log user's guess without for loop
 document.onkeyup = () => {
     let letterCode = String.fromCharCode(event.keyCode);
+    //ran into issues with this function returning the boolean accurately when it came to different case letters. so that's why this is here. 
     let letter = letterCode.toLowerCase();
     //user guess correct
     if (word.indexOf(letter) > -1) {
         // add to correct letter array
         correctLetter.push(letter);
         //change underscore to correct letter
+        //I can't get this to work right on the words that have letters that repeat. it only counts the first instance of the letter.
         underScore[word.indexOf(letter)] = letter;
-        underScore.join('')
+        liveWord.textContent = underScore.join(' ');
         console.log(underScore.join(''));
         //confirms if word matches user guesses
         if (underScore.join('') == word) {
-            alert('you win');
+            alert('win');
+            //alert works but I can't get the win counter to do anything
+            wins++;
+            winCount = 'wins: ' + wins;
         }
+
     }
     else {
         //add to incorrect letter array
         incorrectLetter.push(letter);
+        console.log(incorrectLetter);
+        incLetters.textContent = incorrectLetter;
+
+        //this should have a loss counter 
     }
+
+    //number of guesses left.
+    //function that decreases the amount of attempts you have left everytime you select an incorrect letter
+
+    //the only other thing was to add some css so it doesn't look like garbage. had to actually get it to not run like garbage first though, but that didn't happen. 
+
 }
+
